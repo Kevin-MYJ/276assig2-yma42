cac5c519375402b5accffe7540a8e519cbfeee3f
Yue Ma <wengehuastudy2016@gmail.com>
2019-10-15 15:57:04 -0700

    home

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ var pool;
 pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
